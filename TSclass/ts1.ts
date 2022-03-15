1593635c1943119998577c9a195033feb47e3df2
jayshree01 <jayu9399@gmail.com>
2022-03-16 04:11:54 +0530

    bye

---
+++ TSclass/ts1.ts
@@ -0,0 +1,16 @@
+interface I{
+    addDistance(d1:Distance):Distance;
+
+}
+class Distance {
+    private a:number;
+    private b:number;
+    
+    constructor(a:number,b:number){
+        this.a=a;
+        this.b=b;
+    }
+
+    
+    
+}
